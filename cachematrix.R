7f2af252bfd3f903ed7806fd2da3aae2b219d195
justin <justin@midurth.com>
2015-07-11 20:22:10 +0200

    implement assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains a pair of functions that can be used to 
+## calculate the inverse of a matrix, caching the result so
+## that the inverse will not be recalculated if it is
+## requested again
 
-## Write a short comment describing this function
+## this function returns a closure consisting of
+## 2 data elements (matrices) and accessor functions
+## set and get operate on the first matrix - x
+## set will additionally clear "inverse" when it is called
+## setinverse and getinverse operate on the second matrix ("inverse")
 
 makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
 
+  set <- function(newX) {
+    x <<- newX
+    inverse <<- NULL
+  }
+  
+  get <- function() x
+  
+  setinverse <- function(newInverse) inverse <<- newInverse
+  getinverse <- function() inverse
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## this function calculates the inverse if necessary
+## and handles the caching using the closure above
+## when the inverse is calculated, it is cached in the closure
+## if inverse is requested again, the cached value from the
+## closure will be returned
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if (!is.null((inverse))) {
+    message("returning cached data")
+    return(inverse)
+  }
+  
+  message("calculating inverse")
+  toInvert <- x$get()
+  inverse <- solve(toInvert, ...)
+
+  ## cache here
+  x$setinverse(inverse)
+  inverse
 }
